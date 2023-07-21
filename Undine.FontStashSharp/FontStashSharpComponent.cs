949f3e3e084d1f74bf63db725287b41cbf051b74
Tomasz Cekało <tomasz.cekalo@gmail.com>
2023-07-21 11:57:58 +0200

    initial

---
+++ Undine.FontStashSharp/FontStashSharpComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Undine.FontStashSharp
+{
+    public struct FontStashSharpComponent
+    {
+        public SpriteFont Font { get; internal set; }
+        public string Text { get; internal set; }
+    }
+}
---
+++ Undine.FontStashSharp/FontStashSharpSystem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Undine.Core;
+using Undine.MonoGame;
+using static System.Formats.Asn1.AsnWriter;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Undine.FontStashSharp
+{
+    public class FontStashSharpSystem : UnifiedSystem<FontStashSharpComponent, TransformComponent, ColorComponent>
+    {
+        public FontStashSharpSystem(SpriteBatch spriteBatch)
+        {
+            SpriteBatch = spriteBatch;
+        }
+
+        public SpriteBatch SpriteBatch { get; }
+
+        private Vector2 normalizedOrigin = new Vector2(0.5f, 0.5f);
+
+        public override void ProcessSingleEntity(int entityId, ref FontStashSharpComponent a, ref TransformComponent b, ref ColorComponent c)
+        {
+            var size = a.Font.MeasureString(a.Text) * b.Scale;
+            SpriteBatch.DrawString(a.Font, a.Text, b.Position, c.Color, b.Rotation, size * normalizedOrigin, b.Scale, SpriteEffects.None, 0);
+        }
+    }
+}
